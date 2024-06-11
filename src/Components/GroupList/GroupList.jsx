69bdbb3213eb2f8b92d8dc9f6aeb33fa1cb8877f
Hadeer Magdy <dody.magdy711@gmail.com>
2024-06-12 01:45:19 +0300

    responsive table

--- src/Components/GroupList/GroupList.jsx
+++ src/Components/GroupList/GroupList.jsx
@@ -1,38 +1,41 @@
+
 import React from 'react';
 import Group from '../Group/Group';
-import NoData from '../NoData/NoData'; 
+import NoData from '../NoData/NoData';
 
-const GroupList = ({ groups, updateGroup, deleteGroup, addPost }) => {
+const GroupList = ({ groups, updateGroup, deleteGroup }) => {
   return (
     <div>
-      <h2 className='text-center my-4' style={{color:"#ccc"}}>Groups List</h2>
-      {groups.length === 0 ? ( // Check if groups array is empty
-        <NoData /> // If empty, display the NoData component
+      <h2 className='text-center my-4' style={{ color: "#ccc" }}>Groups List</h2>
+      {groups.length === 0 ? (
+        <NoData />
       ) : (
-        <table className="table mt-3">
-          <thead className="table-head  table-primary">
-            <tr>
-              <th scope="col">#Id</th>
-              <th scope="col">Group Name</th>
-              <th scope="col">Description</th>
-              <th scope="col">Created At</th>
-              <th scope="col">Actions</th>
-              <th scope="col">Posts</th>
-            </tr>
-          </thead>
-          <tbody className='w-100'>
-            {groups.map((group, index) => (
-              <tr key={group.id} className="table-light">
-                <Group
-                  group={group}
-                  updateGroup={updateGroup}
-                  deleteGroup={deleteGroup}
-                  index={index + 1}
-                />
+        <div className="table-responsive"> 
+          <table className="table mt-3">
+            <thead className="table-head table-primary">
+              <tr>
+                <th scope="col">#Id</th>
+                <th scope="col">Group Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Actions</th>
+                <th scope="col">Posts</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className='w-100'>
+              {groups.map((group, index) => (
+                <tr key={group.id} className="table-light">
+                  <Group
+                    group={group}
+                    updateGroup={updateGroup}
+                    deleteGroup={deleteGroup}
+                    index={index + 1}
+                  />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
