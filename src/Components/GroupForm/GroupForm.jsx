74cc63768902c0a0eeb43486c6b9533e4249b3d9
Hadeer Magdy <dody.magdy711@gmail.com>
2024-06-12 01:07:30 +0300

    add alt in img

--- src/Components/GroupForm/GroupForm.jsx
+++ src/Components/GroupForm/GroupForm.jsx
@@ -87,7 +87,7 @@ const GroupForm = ({ addGroup }) => {
                       </form>
                     </div>
                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex justify-content-center align-items-center order-1 order-lg-2">
-                      <img  style={{height:"250px"}}src={img} className="img-fluid" alt="Sample image" />
+                      <img  style={{height:"250px"}}src={img} className="img-fluid" alt="Sampleimage" />
                     </div>
                   </div>
                 </div>
--- src/Components/NoData/NoData.jsx
+++ src/Components/NoData/NoData.jsx
@@ -3,7 +3,7 @@ import noData from "../../assets/imgs/no--data.webp";
 export default function NoData() {
   return (
     <div className="text-center">
-            <img src={noData} className="img-fluid"/>
+            <img src={noData} className="img-fluid" alt="no-data"/>
            <p> No groups Found</p>
            
             </div>
