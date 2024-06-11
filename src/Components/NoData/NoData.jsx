3db95883c706febec0038eeeba88bbf3f5032214
Hadeer Magdy <dody.magdy711@gmail.com>
2024-06-12 01:14:49 +0300

    add alt

--- src/Components/NoData/NoData.jsx
+++ src/Components/NoData/NoData.jsx
@@ -3,7 +3,7 @@ import noData from "../../assets/imgs/no--data.webp";
 export default function NoData() {
   return (
     <div className="text-center">
-            <img src={noData} className="img-fluid" alt="no-data"/>
+            <img src={noData} className="img-fluid" alt="No data available"/>
            <p> No groups Found</p>
            
             </div>
